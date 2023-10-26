e47ff28c68a5fee6aa421755b11758f9ce85f94b
Rômulo Nunes <romulotg21@gmail.com>
2023-10-26 13:38:51 -0300

    fix: some adjusts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import app from "./app";
+import * as dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 8080;
 
-app.listen(3333);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
