b42fb0973314ebbf45b190ce2727b8e181f45957
Rômulo Nunes <romulotg21@gmail.com>
2023-10-26 13:30:39 -0300

    feat: server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,25 @@
 import express from "express";
+import cors from "cors";
 
-const app = express();
+class App {
+  public express: express.Application;
 
-app.listen(3030);
+  public constructor() {
+    this.express = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private routes(): void {
+    this.express.get("/", (req, res) => {
+      return res.send("Hello World");
+    });
+  }
+}
+
+export default new App().express;
