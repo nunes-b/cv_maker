dcbcb1c7e77f61b78219b17701f938271b18ca31
Rômulo Nunes <romulotg21@gmail.com>
2023-10-28 10:34:45 -0300

    chore: limiter

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import rateLimit from "express-rate-limit";
 import bodyparser from "body-parser";
 import routes from "./routes";
+import { limiter } from "./utils/limiter";
 
 class App {
   public app: express.Application;
@@ -20,7 +20,7 @@ class App {
     this.app.use(helmet());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
-    this.app.use(rateLimit());
+    this.app.use(limiter);
     this.app.use(bodyparser.json());
     this.app.use(bodyparser.urlencoded({ extended: false }));
   }
