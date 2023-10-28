1f37523369d1a042afc8c0d1f132020366edcd46
Rômulo Nunes <romulotg21@gmail.com>
2023-10-28 12:17:26 -0300

    fix: schema and migrate

---
+++ prisma/migrations/20231028151534_ajuste_em_curriculo/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "curriculos" ALTER COLUMN "telefone" DROP NOT NULL,
+ALTER COLUMN "perfilLinkedIn" DROP NOT NULL,
+ALTER COLUMN "cidade" DROP NOT NULL,
+ALTER COLUMN "areaAtuacao" DROP NOT NULL;
