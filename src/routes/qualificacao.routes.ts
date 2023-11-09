8a471dc97e8e59124f20d34c64672c4ddbbb9cc5
Rômulo Nunes <romulotg21@gmail.com>
2023-11-09 11:47:57 -0300

    feat: qualificacao list all route

--- src/routes/qualificacao.routes.ts
+++ src/routes/qualificacao.routes.ts
@@ -1,10 +1,14 @@
 import { createQualificacaoController } from "../controller/qualificacao/addQualificacao.controller";
+import { listQualificacao } from "../controller/qualificacao/listQualificacao.controller";
 import { Request, Response, Router } from "express";
 
 const router = Router();
 
 router.post("/qualificacao", async (req: Request, res: Response) => {
   await createQualificacaoController(req, res);
+});
+router.get("/qualificacao", async (req: Request, res: Response) => {
+  await listQualificacao(req, res);
 });
 
 export { router as qualificacaoRoutes };
