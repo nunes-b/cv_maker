4befbd8da0d4841d8fb9976915337cc55af64c16
Rômulo Nunes <romulotg21@gmail.com>
2023-11-10 14:12:57 -0300

    feat: list curso by id

--- src/routes/curso.routes.ts
+++ src/routes/curso.routes.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 import { createCursoController } from "../controller/curso/addCurso.controller";
 import { listCursoController } from "../controller/curso/listCursos.controller";
+import { listCursoByIdController } from "../controller/curso/listCursoById.controller";
+
 const router = Router();
 
 router.post("/curso", async (req: Request, res: Response) => {
@@ -8,6 +10,9 @@ router.post("/curso", async (req: Request, res: Response) => {
 });
 router.get("/curso", async (req: Request, res: Response) => {
   await listCursoController(req, res);
+});
+router.get("/curso/:id", async (req: Request, res: Response) => {
+  await listCursoByIdController(req, res);
 });
 
 export { router as CursoRoutes };
